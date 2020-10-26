d85d7fc7e062edfe40f688754ab9ae4b4e00f95c
Nick Webster <nick@nick.geek.nz>
2020-10-26 21:33:02 +1300

    Updating the library to run on stable rust

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,9 @@
-#![license = "MIT"]
 #![deny(missing_docs, warnings)]
 
 //! Generalized conversion to a target type.
 
 /// A type which can be converted to `X`.
-trait To<X> : Into<X> {
+pub trait To<X> : Into<X> {
     /// Convert self to an instance of `X`.
     fn to(self) -> X {
         self.into()
